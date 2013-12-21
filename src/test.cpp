e03d35cee188d6221e916000b6478e2b160ed2fd
blandw <blandw@gmail.com>
2013-12-21 14:04:23 -0500

    Add new test data.

--- src/test.cpp
+++ src/test.cpp
@@ -10,6 +10,7 @@ std::string setToStr(std::set<int> x) {
   std::set<int>::iterator iter = x.begin();
   if (iter != x.end()) {
     s += intToStr(*iter);
+    iter++;
     for (; iter != x.end(); iter++) {
       s += ", " + intToStr(*iter);
     }
@@ -22,22 +23,24 @@ void timeCPM(const int num_trials /* = 1 */) {
   const std::string test_dir = "../test/";
   
   std::vector< std::string > text_filepaths;
-  text_filepaths.push_back("c5.fa");
-  text_filepaths.push_back("c6.fa");
+  text_filepaths.push_back("t1.fas");
+  // text_filepaths.push_back("c5.fa");
+  // text_filepaths.push_back("c6.fa");
   
   std::vector< std::string > pattern_filepaths;
-  pattern_filepaths.push_back("v1.fas");
-  pattern_filepaths.push_back("v2.fas");
-  pattern_filepaths.push_back("v3.fas");
-  pattern_filepaths.push_back("v4.fas");
-  pattern_filepaths.push_back("v5.fas");
-  pattern_filepaths.push_back("v6.fas");
-  pattern_filepaths.push_back("v7.fas");
-  pattern_filepaths.push_back("v8.fas");
-  pattern_filepaths.push_back("v9.fas");
-  pattern_filepaths.push_back("v10.fas");
+  pattern_filepaths.push_back("p1.fas");
+  // pattern_filepaths.push_back("v1.fas");
+  // pattern_filepaths.push_back("v2.fas");
+  // pattern_filepaths.push_back("v3.fas");
+  // pattern_filepaths.push_back("v4.fas");
+  // pattern_filepaths.push_back("v5.fas");
+  // pattern_filepaths.push_back("v6.fas");
+  // pattern_filepaths.push_back("v7.fas");
+  // pattern_filepaths.push_back("v8.fas");
+  // pattern_filepaths.push_back("v9.fas");
+  // pattern_filepaths.push_back("v10.fas");
 
-  std::cout << "text,pattern,matches\n";
+  std::cout << "text,pattern,time,matches\n";
 
   for (std::vector< std::string >::iterator iter_t = text_filepaths.begin(); iter_t != text_filepaths.end(); iter_t++) {
     std::string text = readFile(test_dir + *iter_t);
@@ -63,7 +66,7 @@ void timeCPM(const int num_trials /* = 1 */) {
       t1 = clock();
       double avg_dt = ((double)(t1 - t0)) / CLOCKS_PER_SEC / num_trials;
 
-      std::cout << *iter_t << "," << *iter_p << "," << setToStr(matches) << ", " << avg_dt << "\n";
+      std::cout << *iter_t << "," << *iter_p << ", " << avg_dt << "," << setToStr(matches) << "\n";
     }
   }
 }
